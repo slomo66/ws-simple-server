ea301d5295247ab5afb728d7f8bf204d68c6c0ac
Kuhl (LOCL) <kuhl01-locl@technotrans.de>
2025-01-16 11:17:33 +0100

    change to express-ws

--- server.js
+++ server.js
@@ -1,49 +1,27 @@
-const WebSocket = require("ws");
-const express = require("express");
+const express = require(`express`)
 const app = express();
-
-const HTML_PORT = process.env.PORT || 8080;
-const WS_PORT = 5000;
+const expressWs = require('express-ws')(app);
 
 
-const wsServer = new WebSocket.Server({
-  port: WS_PORT,
-});
+let lastMsg = "";
 
-// Websockets
-
-wsServer.on("connection", function (socket) {
-  // Some feedback on the console
+app.ws("/", (ws, req) => {
   console.log("A client just connected");
 
-  // Attach some behavior to the incoming socket
-  socket.on("message", function (msg) {
+  ws.on('message', function (msg) {
     console.log("Received message from client: " + msg);
-    // socket.send("Take this back: " + msg);
 
-    // Broadcast that message to all connected clients
+    lastMsg = msg;
+
     const timestamp = Date.now();
-    wsServer.clients.forEach(function (client) {
-      client.send(`Timestamp:${timestamp} Someone said: ${msg}`);
-    });
+    ws.send(`Timestamp:${timestamp} Someone said: ${msg}`);
   });
-
-  socket.on("close", function () {
-    console.log("Client disconnected");
-  });
-});
-
-// Webserver
-
-app.listen(HTML_PORT, () => {
-  console.log("Server started on port", HTML_PORT);
-});
-
-app.get("/", (req, res) => {
-  const timestamp = Date.now();
-  res.send(`Timestamp:${timestamp} Subscribe to Slomo's channel`);
-});
+})
 
 
+app.get(`/abc`, (req, res) => {
+  res.json(`lastMsg: ${lastMsg}`)
+})
 
-console.log(new Date() + " Server is listening on port " + WS_PORT);
+app.listen(8080)
+
