c87ec6f5987db326e6db62ec6bbe0bd7e13a93ee
unknown <kuhl.detlef66@gmail.com>
2025-01-16 13:08:31 +0100

    add fetch weahter data

--- server.js
+++ server.js
@@ -1,14 +1,54 @@
-const express = require(`express`)
+const express = require(`express`);
 const app = express();
-const expressWs = require('express-ws')(app);
+const expressWs = require("express-ws")(app);
+const axios = require("axios");
 
+const port = process.env.PORT || 8080;
 
 let lastMsg = "";
+let objweather = { weather: null, error: null, timestamp: null };
+
+const CITY = "Meinerzhagen";
+const API_KEY = "1cfba33dcfa8499db4b93353250701";
+const BASE_URL = "http://api.weatherapi.com/v1";
+const API_URL = `${BASE_URL}/forecast.json?key=${API_KEY}&q=${CITY}&days=5&lang=de`;
+
+const FETCH_INTERVALL = 15 * 60 * 1000; // 15 Minuten
+
+async function getWeather() {
+  objweather.timestamp = Date.now();
+  try {
+    const response = await axios.get(API_URL);
+    objweather.weather = response.data;
+    objweather.error = null;
+  } catch (err) {
+    objweather.weather = null;
+    objweather.error = err;
+  }
+
+  return await objweather;
+}
+
+async function fetchWeather() {
+  const res = await getWeather();
+
+  if (res.weather != null) {
+    console.log(res.weather?.current);
+  }
+}
+
+
+fetchWeather();
+
+setInterval(() => {
+  fetchWeather();
+}, FETCH_INTERVALL);
+
 
 app.ws("/", (ws, req) => {
   console.log("A client just connected");
 
-  ws.on('message', function (msg) {
+  ws.on("message", function (msg) {
     console.log("Received message from client: " + msg);
 
     lastMsg = msg;
@@ -16,12 +56,17 @@ app.ws("/", (ws, req) => {
     const timestamp = Date.now();
     ws.send(`Timestamp:${timestamp} Someone said: ${msg}`);
   });
-})
-
+});
 
 app.get(`/abc`, (req, res) => {
-  res.json(`lastMsg: ${lastMsg}`)
-})
+  res.json(`lastMsg: ${lastMsg}`);
+});
 
-app.listen(8080)
+app.get("/aktuell", (req, res) => {
+  res.send(objweather?.weather?.current);
+});
 
+//app.listen(port);
+app.listen(port, () => {
+  console.log("Server started on port", port);
+});
